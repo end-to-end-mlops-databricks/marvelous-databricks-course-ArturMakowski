4f018e37d1f39cfe5368759ca836700d9d7c82f9
Artur Makowski <artmak1@st.amu.edu.pl>
2024-11-17 20:53:44 +0100

    fix: train script

--- src/mlops_with_databricks/training/train.py
+++ src/mlops_with_databricks/training/train.py
@@ -35,88 +35,92 @@ def get_git_info():
         return {"git_sha": "unknown", "branch": "unknown"}
 
 
-mlflow.set_tracking_uri("databricks")
-mlflow.set_registry_uri("databricks-uc")  # It must be -uc for registering models to Unity Catalog
+if __name__ == "__main__":
+    mlflow.set_tracking_uri("databricks")
+    mlflow.set_registry_uri("databricks-uc")  # It must be -uc for registering models to Unity Catalog
 
-# COMMAND ----------
+    # COMMAND ----------
 
-# Extract configuration details
-num_features = ProcessedAdClickDataConfig.num_features
-cat_features = ProcessedAdClickDataConfig.cat_features
-target = ProcessedAdClickDataConfig.target
-catalog_name = DatabricksConfig.catalog_name
-schema_name = DatabricksConfig.schema_name
-parameters = light_gbm_config
+    # Extract configuration details
+    num_features = ProcessedAdClickDataConfig.num_features
+    cat_features = ProcessedAdClickDataConfig.cat_features
+    target = ProcessedAdClickDataConfig.target
+    catalog_name = DatabricksConfig.catalog_name
+    schema_name = DatabricksConfig.schema_name
+    parameters = light_gbm_config
 
-# COMMAND ----------
-spark = SparkSession.builder.getOrCreate()
+    # COMMAND ----------
+    spark = SparkSession.builder.getOrCreate()
 
-# Load training and testing sets from Databricks tables
-train_set_spark = spark.table(f"{catalog_name}.{schema_name}.train_set")
-train_set = spark.table(f"{catalog_name}.{schema_name}.train_set").toPandas()
-test_set = spark.table(f"{catalog_name}.{schema_name}.test_set").toPandas()
-print(train_set.head())
-X_train = train_set[list(num_features) + list(cat_features)]
-y_train = train_set[target]
+    # Load training and testing sets from Databricks tables
+    train_set_spark = spark.table(f"{catalog_name}.{schema_name}.train_set")
+    train_set = spark.table(f"{catalog_name}.{schema_name}.train_set").toPandas()
+    test_set = spark.table(f"{catalog_name}.{schema_name}.test_set").toPandas()
+    print(train_set.head())
+    X_train = train_set[list(num_features) + list(cat_features)]
+    y_train = train_set[target]
 
-X_test = test_set[list(num_features) + list(cat_features)]
-y_test = test_set[target]
+    X_test = test_set[list(num_features) + list(cat_features)]
+    y_test = test_set[target]
 
-# COMMAND ----------
-# Define the preprocessor for categorical features
-preprocessor = ColumnTransformer(
-    transformers=[("cat", OneHotEncoder(handle_unknown="ignore"), cat_features)], remainder="passthrough"
-)
+    # COMMAND ----------
+    # Define the preprocessor for categorical features
+    preprocessor = ColumnTransformer(
+        transformers=[("cat", OneHotEncoder(handle_unknown="ignore"), cat_features)], remainder="passthrough"
+    )
 
-# Create the pipeline with preprocessing and the LightGBM regressor
-pipeline = Pipeline(steps=[("onehot", preprocessor), ("classifier", LGBMClassifier(**parameters))])
+    # Create the pipeline with preprocessing and the LightGBM regressor
+    pipeline = Pipeline(steps=[("onehot", preprocessor), ("classifier", LGBMClassifier(**parameters))])
 
-# Define parameter grid for hyperparameter tuning
-param_grid = {
-    "classifier__learning_rate": [0.001, 0.01, 0.1],
-    "classifier__n_estimators": [100, 200, 300, 400],
-    "classifier__max_depth": [5, 10, 15, 20],
-}
+    # Define parameter grid for hyperparameter tuning
+    param_grid = {
+        "classifier__learning_rate": [0.001, 0.01, 0.1],
+        "classifier__n_estimators": [100, 200, 300, 400],
+        "classifier__max_depth": [5, 10, 15, 20],
+    }
 
-# Perform hyperparameter tuning with GridSearchCV
-grid_search = GridSearchCV(pipeline, param_grid, cv=4, scoring="f1", n_jobs=-1)
+    # Perform hyperparameter tuning with GridSearchCV
+    grid_search = GridSearchCV(pipeline, param_grid, cv=4, scoring="f1", n_jobs=-1)
 
-# COMMAND ----------
-mlflow.set_experiment(experiment_name="/Shared/ad-click")
+    # COMMAND ----------
+    mlflow.set_experiment(experiment_name="/Shared/ad-click")
 
-# Start an MLflow run to track the training process
-with mlflow.start_run(
-    tags=get_git_info(),
-) as run:
-    run_id = run.info.run_id
+    # Start an MLflow run to track the training process
+    with mlflow.start_run(
+        tags=get_git_info(),
+    ) as run:
+        run_id = run.info.run_id
 
-    grid_search.fit(X_train, y_train)
-    best_pipeline = grid_search.best_estimator_
-    best_params = grid_search.best_params_
-    y_pred = best_pipeline.predict(X_test)
+        grid_search.fit(X_train, y_train)
+        best_pipeline = grid_search.best_estimator_
+        best_params = grid_search.best_params_
+        y_pred = best_pipeline.predict(X_test)
 
-    # Evaluate the model performance
-    f1 = f1_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
+        # Evaluate the model performance
+        f1 = f1_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        roc_auc = roc_auc_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)
 
-    # Log parameters, metrics, and the model to MLflow
-    mlflow.log_param("model_type", "LightGBM with preprocessing")
-    mlflow.log_params(best_params)
-    mlflow.log_metrics({"f1": f1, "accuracy": accuracy, "precision": precision, "recall": recall, "roc_auc": roc_auc})
-    signature = infer_signature(model_input=X_test, model_output=y_pred)
+        # Log parameters, metrics, and the model to MLflow
+        mlflow.log_param("model_type", "LightGBM with preprocessing")
+        mlflow.log_params(best_params)
+        mlflow.log_metrics(
+            {"f1": f1, "accuracy": accuracy, "precision": precision, "recall": recall, "roc_auc": roc_auc}
+        )
+        signature = infer_signature(model_input=X_test, model_output=y_pred)
 
-    dataset = mlflow.data.from_spark(train_set_spark, table_name=f"{catalog_name}.{schema_name}.train_set", version="0")
-    mlflow.log_input(dataset, context="training")
+        dataset = mlflow.data.from_spark(
+            train_set_spark, table_name=f"{catalog_name}.{schema_name}.train_set", version="0"
+        )
+        mlflow.log_input(dataset, context="training")
 
-    mlflow.sklearn.log_model(sk_model=best_pipeline, artifact_path="lightgbm-pipeline-model", signature=signature)
+        mlflow.sklearn.log_model(sk_model=best_pipeline, artifact_path="lightgbm-pipeline-model", signature=signature)
 
-
-# COMMAND ----------
-model_version = mlflow.register_model(
-    model_uri=f"runs:/{run_id}/lightgbm-pipeline-model",
-    name=f"{catalog_name}.{schema_name}.ad_click_model_basic",
-    tags=get_git_info(),
-)
+    # COMMAND ----------
+    model_version = mlflow.register_model(
+        model_uri=f"runs:/{run_id}/lightgbm-pipeline-model",
+        name=f"{catalog_name}.{schema_name}.ad_click_model_basic",
+        tags=get_git_info(),
+    )
